341e5c3f3682551f238ade1a677c26e7573e6b3a
Oleksii Plichko <oplichko@splitdev.pro>
2022-10-12 10:58:06 +0300

    12.10.22 Coming soon, Cart

--- assets/global.js
+++ assets/global.js
@@ -1056,3 +1056,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
   window.firstLoad = false;
 });
+
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
